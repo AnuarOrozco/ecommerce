43812b8cc4f11011c85d07198292d366f7cc39cc
Anuar <anuarorozzco@gmail.com>
2024-10-12 18:54:47 -0600

    Created the packages and the classes

--- ecommerce-platform/ecommerce-platform/src/main/java/mx/uvm/anuar/ecommerce_platform/EcommercePlatformAnuarApplication.java
+++ ecommerce-platform/ecommerce-platform/src/main/java/mx/uvm/anuar/ecommerce_platform/EcommercePlatformAnuarApplication.java
@@ -2,8 +2,9 @@ package mx.uvm.anuar.ecommerce_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class EcommercePlatformAnuarApplication {
 
 	public static void main(String[] args) {
