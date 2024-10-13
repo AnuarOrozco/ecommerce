b3848e270983a028070de40aa0c3e4b3c2968ff3
Anuar <anuarorozzco@gmail.com>
2024-10-12 18:26:43 -0600

    Initialize Spring Boot project for e-commerce platform

---
+++ ecommerce-platform/ecommerce-platform/src/main/java/mx/uvm/anuar/ecommerce_platform/EcommercePlatformAnuarApplication.java
@@ -0,0 +1,13 @@
+package mx.uvm.anuar.ecommerce_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePlatformAnuarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePlatformAnuarApplication.class, args);
+	}
+
+}
---
+++ ecommerce-platform/ecommerce-platform/src/test/java/mx/uvm/anuar/ecommerce_platform/EcommercePlatformAnuarApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uvm.anuar.ecommerce_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePlatformAnuarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
